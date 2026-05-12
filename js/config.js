20522c6110c6b37bf4cc130e7df4be1dc07a3e01
shoppycoke <barthsami@gmail.com>
2026-05-13 00:02:24 +0200

    fix compte 1

--- js/config.js
+++ js/config.js
@@ -6,5 +6,5 @@
 // 3. Remplacez les deux valeurs ci-dessous
 // 4. Exécutez supabase-schema.sql dans l'éditeur SQL
 // =====================================================
-window.SUPABASE_URL      = 'https://VOTRE_PROJECT_ID.supabase.co';
-window.SUPABASE_ANON_KEY = 'VOTRE_CLE_ANONYME_SUPABASE';
+window.SUPABASE_URL      = 'https://kqzsyogjhcrrmvqvzvah.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_tTd8qHrnsxKxMCRgZYu7Ug_TPobNiN7';
